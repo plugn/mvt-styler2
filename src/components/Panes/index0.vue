e29a5351a65ec48e625cd9b39201312c48d99818
Maxim Dolgov <bananafishbone@gmail.com>
2017-03-10 20:52:08 +0300

    layout #2 attempt

---
+++ src/components/Panes/index0.vue
@@ -0,0 +1,134 @@
+<template>
+	<div class="panes">
+
+		<div class="view">
+			<div class="sidebar resizable"  v-resize="{height:false}" data-handle="layers-resizer">
+				<div class="toolbar gu-unselectable">
+					<span @click="setSideBar('tree')">Layers</span>
+					<span @click="setSideBar('code')">Code</span>
+				</div>
+				<div class="widget">
+					<LayerTree v-show="isActive('tree')"></LayerTree>
+
+					<Editor v-show="isActive('code')" class="body"></Editor>
+
+					<div v-show="isActive('tweakLayer')">
+						Tweak {{sideBar}} #{{layerId}}
+						<div ref="dat">
+							BLA
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="pane"></div>
+		</div>
+
+<!--
+		<div class="resizable scrollable-y" v-resize="{height:false}" data-handle="layers-resizer">
+			<div class="toolbar gu-unselectable">
+				<span @click="setSideBar('tree')">Layers</span>
+				<span @click="setSideBar('code')">Code</span>
+			</div>
+
+			<LayerTree v-show="isActive('tree')"></LayerTree>
+			<Editor v-show="isActive('code')" class="body"></Editor>
+			<div v-show="isActive('tweakLayer')">
+				Tweak {{sideBar}} #{{layerId}}
+				<div ref="dat">
+					BLA
+
+
+				</div>
+			</div>
+		</div>
+-->
+		<div class="el-resizer" ref="layers-resizer"></div>
+
+		<div class="pane">
+			<MapGL></MapGL>
+		</div>
+	</div>
+
+</template>
+
+<script>
+	import resize from '../../directives/resize'
+	import LayerTree from '../LayerList/LayerTree.vue'
+	import Editor from '../Editor'
+	import MapGL from '../Map'
+	import {eventBus} from '../../main';
+	import dat from 'dat.gui/build/dat.gui';
+	import dgLightTheme from 'dat-gui-light-theme/dat-gui-light-theme.css'
+	import dgTweak from './dg-tweak.css'
+
+console.log('dat', dat);
+
+	export default {
+		components: {
+			LayerTree,
+			MapGL,
+			Editor
+		},
+		directives: {
+			resize
+		},
+		data() {
+			return {
+				sideBar: 'tree',
+				layerId: ''
+			}
+		},
+
+		mounted() {
+
+			eventBus.$on('tweakLayer', (layerId) => {
+				console.log('caught tweakLayer', layerId)
+				let data = LayerTree.methods.getLayer(layerId);
+				console.log('LayerTree', data);
+				this.layerId = layerId;
+				this.setSideBar('tweakLayer');
+
+				let gui = new dat.gui.GUI({autoPlace: false});
+
+//				gui.remember(data);
+				gui.add(data, 'id');
+				gui.add(data, 'interactive');
+
+//				console.log('dat', this.$refs.dat);
+				let container = this.$refs.dat;
+				if (container) {
+					container.innerHTML = '';
+					container.appendChild(gui.domElement);
+				}
+
+			})
+		},
+
+		methods: {
+			onResize: function () {
+				// console.log('onResize');
+				setTimeout( (function () {
+					eventBus.$emit('map:resize');
+				}), 100);
+			},
+			setSideBar(name) {
+				this.sideBar = name;
+
+
+			},
+			isActive(name) {
+				return name === this.sideBar;
+			}
+
+
+		}
+
+
+
+	}
+
+</script>
+
+<style src="./Panes.css" scoped></style>
+<!--<style src="dat-gui-light-theme/dat-gui-light-theme.css"></style>-->
