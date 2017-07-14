a18cf6f2c572e4676ef992c04edf892ae2807d13
Maxim Dolgov <bananafishbone@gmail.com>
2017-07-14 21:10:33 +0300

    group styled up

--- src/components/LayerList/LayerTreeItem.vue
+++ src/components/LayerList/LayerTreeItem.vue
@@ -4,7 +4,33 @@
 		<LayerTreeItem :model="listItem" :itemIndex="listKey" v-for="(listItem, listKey) in model"></LayerTreeItem>
 	</ul>
 
-	<li v-else-if="isFolder" class="tile tile__folder">
+	<li v-else-if="isFolder" class="col12 clearfix contain dark">
+		<div class="animate false  draggable layer-group"><a
+				class="pin-topleft z1 icon caret-down pad00y"></a>
+			<div title="Group"
+				 class="keyline-bottom keyline-dark2 layer-folder block pad0x pad00y pointer micro"><span
+					class="pin-topleft z1 icon inline folder" style="left: 12px; top: 2px;"></span>
+				<div class="pad0x micro" style="padding-left: 27px;">
+					<div class="contain col12 ">
+						<div class="truncate layer-folder-title"><span data-test="">{{ model.id }} {{open ? '-' : '+'}}</span><span
+								class="quiet space-left0">{{model.children.length}} layers</span>
+							<button class="pin-topright animate icon"></button>
+						</div>
+					</div>
+				</div>
+				<span class="pin-topright z1 drag-handle animate"></span>
+			</div>
+		</div>
+		<ul>
+			<ListGroupItem
+					v-for="(listItem, listKey) in model.children"
+					:model="listItem"
+					:itemIndex="listKey"
+					:data-group="itemIndex"></ListGroupItem>
+		</ul>
+	</li>
+
+	<li v-else-if="0 && isFolder" class="tile tile__folder">
 		<span class="tile__name bold" @click.self="toggle">{{ model.id }} [ {{open ? '-' : '+'}} ]</span>
 		<ul v-show="open" class="draghost tile__list" :data-group="itemIndex">
 			<li hidden></li>
@@ -16,6 +42,8 @@
 		</ul>
 	</li>
 
+
+
 	<ListItem v-else :model="model"></ListItem>
 
 	<!--<li v-else class="tile" @click.self="tweakLayer(model.id)">{{model.id}}</li>-->
@@ -26,10 +54,15 @@
 <script>
 	import {eventBus} from '../../main'
 	import ListItem from  './ListItem.vue'
+	import ListGroupItem from './ListGroupItem.vue'
+
 
 	export default {
 		name: 'LayerTreeItem',
-		components: {ListItem},
+		components: {
+			ListItem,
+			ListGroupItem
+		},
 		props: {
 			model: [Object, Array],
 			itemIndex: [Number]
@@ -37,7 +70,7 @@
 
 		data() {
 			return {
-				open: false
+				open: true
 			}
 		},
 
