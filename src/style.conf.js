98d407635f13536931ccabc7bffd3773558e5230
Maxim Dolgov <bananafishbone@gmail.com>
2017-08-29 19:59:16 +0300

    style set to bright again

--- src/style.conf.js
+++ src/style.conf.js
@@ -1,5 +1,5 @@
 //	import mbStyle from './res/standard/style.json'
 // import mbStyle from './res/sputnik2g1.json'
-// import mbStyle from './res/bright-v9ru.json'
-import mbStyle from './res/lite-v9ru.json'
+// import mbStyle from './res/lite-v9ru.json'
+import mbStyle from './res/bright-v9ru.json'
 export default mbStyle;
